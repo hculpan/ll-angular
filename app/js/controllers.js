df1e8d5db68e4c8480030f31ab492741dafbf704
Harry Culpan <harry@tesseract.org>
2014-03-23 11:55:20 -0400

    Now can load individual monsters

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,22 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', ['$scope', 'Monsters', function($scope, Monsters) {
-        $scope.monsters = Monsters.query();
+  controller('MonstersCtrl', ['$scope', 'Monsters', 'MonstersCache', function($scope, Monsters, MonstersCache) {
+        var data = MonstersCache.get('list')
+        if (data) {
+            $scope.monsters = data
+        } else {
+            $scope.monsters = Monsters.query()
+            MonstersCache.put('list', $scope.monsters)
+        }
   }])
-  .controller('MyCtrl2', [function() {
-
+  .controller('MonsterCtrl', ['$scope', '$routeParams', 'Monster', 'MonstersCache', function($scope, $routeParams, Monster, MonstersCache) {
+        var data = MonstersCache.get($routeParams.monsterName)
+        if (data) {
+            console.log('retrieve ' + $routeParams.monsterName + ' from cache')
+            $scope.monster = data
+        } else {
+            $scope.monster = Monster($routeParams.monsterName).get()
+            MonstersCache.put($routeParams.monsterName, $scope.monster)
+        }
   }]);
--- app/js/services.js
+++ app/js/services.js
@@ -2,15 +2,40 @@
 
 /* Services */
 
-
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', ['ngResource'])
+    .factory('MonstersCache', function($cacheFactory) {
+        return $cacheFactory('MonstersCache', {})
+    })
     .factory('Monsters', ['$resource',
         function($resource) {
-            return $resource('https://o5YdoMQL3vAkgZmrebowBWYknxPFAHI1xm0msyIY:javascript-key=F0TKDNUeFLP9DT5iWWZb3QUDVPQD8y8wKWd7DDd1@api.parse.com/1/classes/monsters', {}, {
-                query: {method: 'GET', isArray: true}
+            return $resource('https://api.parse.com/1/classes/monsters', {keys: 'name,hd', limit: '1000'}, {
+                query: {
+                    method: 'GET',
+                    headers: {
+                        'X-Parse-Application-Id':'o5YdoMQL3vAkgZmrebowBWYknxPFAHI1xm0msyIY',
+                        'X-Parse-REST-API-Key':'WgYxkAzAAFRVHCGScT9dZkKwN1COuFazaxzbm4ex'
+                    }
+                }
             })
+        }
+    ])
+    .factory('Monster', ['$resource',
+        function($resource) {
+            return function(monsterName) {
+                return $resource('https://api.parse.com/1/classes/monsters', {where: '{"name": "' + monsterName + '"}'}, {
+                    get: {
+                        method: 'GET',
+                        isArray: false,
+                        responseType: 'json',
+                        headers: {
+                            'X-Parse-Application-Id':'o5YdoMQL3vAkgZmrebowBWYknxPFAHI1xm0msyIY',
+                            'X-Parse-REST-API-Key':'WgYxkAzAAFRVHCGScT9dZkKwN1COuFazaxzbm4ex'
+                        }
+                    }
+                })
+            }
         }
     ])
     .value('version', '0.1');
